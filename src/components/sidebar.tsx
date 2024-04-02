ccdc6220f52daec3c67d8b71732cda1829fbda34
JosephMugo8711 <wamiti8711@gmail.com>
2024-04-02 12:13:50 +0300

    Feat: readme, sidebar

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,14 @@
+export function Sidebar(){
+    return(
+        <aside className="w-[270px] max-w-xs h-screen fixed left-0 top-0 z-40 border-r">
+            <div className="h-full px-3 py-4">
+                <h3 className="mx-3 text-lg font-semibold text-foreground">
+                    Twitter
+
+                </h3>
+
+            </div>
+
+        </aside>
+    )
+}
